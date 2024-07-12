b2d224a91b6cf108818e827efe0b2f842a75552c
jamesbasa <jamesbasanmhs@gmail.com>
2024-07-12 14:56:49 -0700

    add for loop to render all users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+import { UserComponent } from './user/user.component';
+import { CommonModule } from '@angular/common';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, UserComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-angular-app';
+
+  users: any[] = [];
+
+  ngOnInit() {
+    this.users = DUMMY_USERS;
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,20 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  @Input() currentUser: any;
+
+  get imagePath() {
+    return 'assets/users/' + this.currentUser.avatar;
+  }
+
+  onSelectUser() {
+    console.log(`clicked ${this.currentUser.name}`);
+  }
 }
