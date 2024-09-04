adb202f23ecd020998a8443bc18e93fdc2976797
jamesbasa <jamesbasanmhs@gmail.com>
2024-09-03 18:16:05 -0700

    render new TaskComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
+import { TaskComponent } from './task/task.component';
 import { CommonModule } from '@angular/common';
 import { DUMMY_USERS } from './dummy-users';
 
@@ -10,12 +11,19 @@ import { DUMMY_USERS } from './dummy-users';
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, HeaderComponent, UserComponent, CommonModule],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TaskComponent,
+    CommonModule,
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'first-angular-app';
 
   users: any[] = [];
+  selectedUserId: any;
 
   ngOnInit() {
     this.users = DUMMY_USERS;
@@ -25,7 +33,11 @@ export class AppComponent implements OnInit {
     return 'assets/users/' + user.avatar;
   }
 
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId);
+  }
+
   onSelectUser(id: any) {
-    console.log(`clicked user with id ${id}`);
+    this.selectedUserId = id;
   }
 }
