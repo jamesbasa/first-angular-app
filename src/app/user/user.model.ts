0a076d900f15ebfbccb23850e341f26148c62820
jamesbasa <jamesbasanmhs@gmail.com>
2024-09-06 16:52:33 -0700

    move User type into new file

---
+++ src/app/user/user.model.ts
@@ -0,0 +1,12 @@
+export type User = {
+  id: string;
+  name: string;
+  avatar: string;
+};
+
+// interface, as opposed to type, is restricted to defining object types
+// export interface User {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
