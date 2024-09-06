958264624df5a4004def32fbd209fcc7eb8f4adf
jamesbasa <jamesbasanmhs@gmail.com>
2024-09-06 16:59:05 -0700

    import and use dummy-tasks

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NgIf, NgFor } from '@angular/common';
+import { TaskComponent } from '../task/task.component';
+import { DUMMY_TASKS } from '../dummy-tasks';
+import { Task } from '../task/task.model';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   @Input({ required: true }) selectedUser: any;
+
+  tasks: Task[] = [];
+
+  ngOnInit() {
+    this.tasks = DUMMY_TASKS;
+  }
+
+  get tasksOfSelectedUser() {
+    return this.tasks.filter(
+      (task: { userId: any }) => task.userId === this.selectedUser.id
+    );
+  }
 }
