8cbe8f1b5e7a153da6dbc6b5a82584f90f58213d
jamesbasa <jamesbasanmhs@gmail.com>
2024-09-06 16:23:27 -0700

    put task behind ngIf

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
 })
