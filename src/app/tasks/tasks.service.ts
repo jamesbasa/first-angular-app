40c331e87654dde72b73c620ed8ca008b7a63e04
jamesbasa <jamesbasanmhs@gmail.com>
2024-10-06 22:53:30 -0700

    use localStorage for task data

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -2,10 +2,20 @@ import { DUMMY_TASKS } from '../dummy-tasks';
 import { Task, TaskInput } from './task/task.model';
 import { Injectable } from '@angular/core';
 
+const STORAGE_KEY_TASKS = 'tasks';
+
 // registers as Injectable so it can be used as a dependency
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  private tasks: Task[] = DUMMY_TASKS;
+  private tasks: Task[] = [];
+
+  constructor() {
+    // below uses browser data saved across browser sessions
+    const tasks = localStorage.getItem(STORAGE_KEY_TASKS);
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
 
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
@@ -19,9 +29,15 @@ export class TasksService {
       summary: taskInput.summary,
       dueDate: taskInput.dueDate,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem(STORAGE_KEY_TASKS, JSON.stringify(this.tasks));
   }
 }
